cdf9f9e0086d8d326a1a4ed730051c5135c04a8a
Roman Jelinek <jelinek82@gmail.com>
2024-02-13 14:16:44 +0100

    Nav logo fix

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -12,13 +12,13 @@ export default function Nav() {
   return (
     <header className="fixed top-0 left-0 right-0 bg-background z-10">
       <nav className="flex justify-between py-2 lg:pt-10 lg:pb-4 px-2 sm:px-4 md:px-8 xl:px-24">
-        <Link href="/" className="text-4xl font-extrabold lg:text-5xl">
+        <Link href="/" className="">
           <Image
             src="/logo.svg"
             alt="logo"
             width={0}
             height={0}
-            style={{ width: "150px", height: "auto" }}
+            style={{ width: "120px", height: "auto" }}
             priority={true}
           />
         </Link>
@@ -26,19 +26,19 @@ export default function Nav() {
           <div className="flex text-4xl items-center">
             <>
               {path != "/eshop" ? (
-                <div className="flex flex-col items-center ml-6 mr-3 md:mr-0">
+                <div className="flex flex-col items-center ml-2 lg:ml-6 mr-3 md:mr-0">
                   <User />
                   <span className="text-xs opacity-80">From czech s.r.o.</span>
                 </div>
               ) : (
                 <>
-                  <div className="flex flex-col items-center ml-6 mr-3 md:mr-0">
+                  <div className="flex flex-col items-center ml-2 lg:ml-6 md:mr-0">
                     <User />
-                    <span className="text-xs opacity-80">
+                    <span className="text-center text-xs opacity-80">
                       From czech s.r.o.
                     </span>
                   </div>
-                  <div className="ml-6 mr-3 md:mr-0">
+                  <div className=" ml-2 lg:ml-6 mr-3 md:mr-0">
                     <Basket />
                   </div>
                 </>
