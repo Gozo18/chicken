27997529db9e36cd1c6d61407620d03032592732
Roman Jelinek <jelinek82@gmail.com>
2024-02-16 14:24:46 +0100

    Toast success styling fix

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -28,7 +28,7 @@ const toastVariants = cva(
     variants: {
       variant: {
         default: "border bg-background text-foreground",
-        secondary: "border bg-success text-success-foreground",
+        secondary: "border-success bg-success text-success-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
       },
